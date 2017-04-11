1b30b328b1a2b3581120e1efad56e222e3fe7dcc
caperaven@icloud.com <caperaven@icloud.com>
2017-04-11 23:34:32 +0200

    icons page

--- src/views/icons/icons.js
+++ src/views/icons/icons.js
@@ -1,15 +1,32 @@
-import {bindable} from 'aurelia-framework';
-import schemaTemplate from './../../../../templates/schema/screen-template.json.template!text';
+import {inject, bindable} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import template from './../../../../screen-templates/icons.json!text';
+import {DynamicViewLoader} from 'pragma-views/lib/dynamic-view-loader';
+import {TemplateParser} from 'pragma-views/lib/template-parser';
+import assistHtml from './icons-assist.html!text';
 
+const SVGO = require('svgo');
+
+@inject(TemplateParser, DynamicViewLoader, EventAggregator)
 export class Icons {
-    @bindable svgCode;
+    @bindable svgText;
 
-    svgCodeChanged() {
-        console.log(svgCode);
+    constructor(templateParser, dynamicViewLoader, eventAggregator) {
+        this.templateParser = templateParser;
+        this.dynamicViewLoader = dynamicViewLoader;
+        this.eventAggregator = eventAggregator;
+    }
+
+    svgTextChanged() {
+        console.log(this.svgText);
     }
 
     cleanSVG() {
-        console.log("clean svg");
+        const svgo = new SVGO();
+
+        svgo.optimize(this.svgText, result => {
+            this.svgText = result.data;
+        });
     }
 
     addNewIcon() {
@@ -18,5 +35,16 @@ export class Icons {
 
     updateIcons() {
         console.log("update icon");
+    }
+
+    attached() {
+        this.templateParser.parse(JSON.parse(template)).then(result => {
+            this.dynamicViewLoader.load(result, this.genContainer, this);
+        });
+
+        this.eventAggregator.publish("assistant", {
+            view: assistHtml,
+            viewModel: this
+        })
     }
 }
